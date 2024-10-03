3efea51a87de7f499d3d6c46b0aeb26d703c815f
VishwaRamesh <118626171+Vishwa2806@users.noreply.github.com>
2024-10-03 13:25:32 +0530

    Add files via upload
    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CardContainer from './card';  // Import the CardContainer (or Card) component
+
+function App() {
+  return (
+    <div className="App">
+      <CardContainer />  {/* Use the correct component name */}
+    </div>
+  );
+}
+
+export default App;
+
